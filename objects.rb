ad698d6e1cd39478bb155cdb3e3a2253194bef06
Dan Mulvihill <dmulvihill3@gmail.com>
2018-08-24 00:22:24 -0400

    fixed bug

--- objects.rb
+++ objects.rb
@@ -25,24 +25,27 @@ cal = Calculator.new(3,7.0)
 puts cal.divide
 
 
+
 class Elevator
     def initialize(current_level)
         @floor = current_level
     end
 
     def cheery_greeting
-        puts "Welcome to floor "
+        puts "Welcome to floor #{@floor} "
     end
 
-    def go_up=(increase)
-        if @floor <= 12
-        @floor += @floor
-        else @floor = 12
+    def go_up
+        if @floor < 12
+            @floor += 1
+        else
+            @floor = 12
+        end
     end
 
-    def go_down=(decrease)
-        if @floor >=1 
-            @floor -= @floor
+    def go_down
+        if @floor >1 
+            @floor -= 1
         else 
             @floor = 1
         end
@@ -52,5 +55,9 @@ end
 passenger1 = Elevator.new(1)
 
 passenger1.cheery_greeting
+#  passenger1.go_down
 passenger1.go_up
 passenger1.cheery_greeting
+passenger1.go_down
+passenger1.go_down  #this should not work
+passenger1.cheery_greeting  
